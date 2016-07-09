1ff4d0d22a8fa544117ac39604698fba306515b5
Cristian Hernandez <ram.230813@outlook.es>
2016-07-09 10:27:37 -0500

    se incorpora utileria string

---
+++ src/com/qacg/utils/StringUtils.java
@@ -0,0 +1,17 @@
+package com.qacg.utils;
+
+public class StringUtils {
+
+	/**
+	 * @param args
+	 */
+	public static String toLowerCase(String s) {
+		// TODO Auto-generated method stub
+		if (s != null) {
+			return s.toLowerCase();
+		} else {
+			return "";
+		}
+	}
+
+}
--- src/com/qsg/main.java
+++ src/com/qsg/main.java
@@ -1,7 +1,10 @@
 package com.qsg;
 
+import com.qacg.utils.StringUtils;
+
 public class main {
 public static void main(String[] args){
-	System.out.println("hello world");
+	String s = "Hello World!!!";
+	System.out.println(StringUtils.toLowerCase(s));
 }
 }
