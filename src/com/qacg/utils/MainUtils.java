cba5c18b3f83df58007bdf870622745d80c81494
Cristian Hernandez <ram.230813@outlook.es>
2016-07-09 10:44:47 -0500

    se agrego el correo

---
+++ src/com/qacg/utils/MainUtils.java
@@ -0,0 +1,9 @@
+package com.qacg.utils;
+
+public class MainUtils {
+
+	public static boolean send(String s){
+		System.out.println(s);
+		return true;
+	}
+}
--- src/com/qsg/main.java
+++ src/com/qsg/main.java
@@ -1,7 +1,10 @@
 package com.qsg;
 
+import com.qacg.utils.MainUtils;
+
 public class main {
 public static void main(String[] args){
 	System.out.println("hello world");
+	MainUtils.send("hercarr.mx@");
 }
 }
